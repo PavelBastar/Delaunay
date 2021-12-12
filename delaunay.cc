c08939f7c0f23ed86f0be2252aef7525d1446619
nemmi1001 <91687500+nemmi1001@users.noreply.github.com>
2021-12-12 19:18:13 +0100

    Add files via upload

--- delaunay.cc
+++ delaunay.cc
@@ -1,17 +1,9 @@
 #include "deluanay.hh"
-#include "utils.hh"
-#include "vertex.hh"
-#include "edge.hh"
-#include "quadedge.hh"
 #include <tuple>
-#include <vector>
-#include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-#include <functional>
-#include <memory>
-#include <stdbool.h>
+#include <unordered_set>
 
 
 
@@ -19,7 +11,7 @@
 void Delaunay::initializeVertices(double low_bound, double up_bound, int N)
 {
     std::ofstream myfile;
-    myfile.open("coordinates.txt");
+    myfile.open("./data/coordinates.txt");
     std::vector<double> x_coords = getRandomVector(low_bound, up_bound, N);
     std::sort(x_coords.begin(), x_coords.end());
     std::vector<double> y_coords = getRandomVector(low_bound, up_bound, N);
@@ -29,9 +21,7 @@ void Delaunay::initializeVertices(double low_bound, double up_bound, int N)
         x = x_coords[i];
         y = y_coords[i];
         myfile << x << ' ' << y << std::endl;
-        Vertex* v = nullptr;
-        v = Vertex::make(x,y);
-        vertices.push_back(v);
+        subdivision->createVertex(x, y);
     }
     myfile.close();
 }
@@ -39,18 +29,12 @@ void Delaunay::initializeVertices(double low_bound, double up_bound, int N)
 
 Delaunay::Delaunay(double low_bound, double up_bound, int N)
 {
-    edges = QuadEdgeList();
+    subdivision = new VertexEdgeManager();
+    file_index = 0;
     initializeVertices(low_bound, up_bound, N);
 }
 
-void Delaunay::kill(Edge* e)
-{
-    Edge::splice(e, e->Oprev());
-    Edge::splice(e->Sym(), e->Sym()->Oprev());
-    QuadEdge* free_edge = (QuadEdge*) (e - (e->getIndex()));
-    edges.erase(std::remove(edges.begin(), edges.end(), free_edge));
-    delete free_edge;
-}
+
 
 VertexHalves Delaunay::SplitVertices(const VertexList& points)
 {
@@ -62,29 +46,12 @@ VertexHalves Delaunay::SplitVertices(const VertexList& points)
 
 Edge* Delaunay::connectTwoVertices(int first, int second, const VertexList& points)
 {
-    Edge* e = Edge::make(edges);
-
-    e->setOrg(points[first]);
-
-    e->setDest(points[second]);
+    Edge* e = subdivision->createEdge(points[first], points[second]);
 
     return e;
 }
 
-Edge* Delaunay::connect(Edge* a, Edge* b)
-{
-   Edge* e = Edge::make(edges);
-   e->setOrg(a->Dest());
 
-   e->setDest(b->Org());
-
-   Edge::splice(e, a->Lnext());
-
-   Edge::splice(e->Sym(), b);
-
-   return e;
-
-}
 
 EdgeHalves Delaunay::makeLine(const VertexList& points)
 {
@@ -104,14 +71,14 @@ EdgeHalves Delaunay::makeTriangle(const VertexList& points)
     if (CCW(points[0], points[1], points[2]))
     {
 
-        Edge* c = connect(b, a);
+        Edge* c = subdivision->connect(b, a);
         return EdgeHalves( { a }, { b->Sym() });
 
     }
     else if (CCW(points[0], points[2], points[1]))
     {
 
-        Edge* c = connect(b, a);
+        Edge* c = subdivision->connect(b, a);
         return EdgeHalves({ c->Sym()}, { c });
 
     }
@@ -148,7 +115,7 @@ Edge* Delaunay::lowestTangentLoop(Edge*& left_inner, Edge*& right_inner)
         }
 
     }
-    Edge* base = connect(right_inner->Sym(), left_inner);
+    Edge* base = subdivision->connect(right_inner->Sym(), left_inner);
     return base;
 
 }
@@ -166,7 +133,7 @@ Edge* Delaunay::lCand(Edge* base)
         {
 
             Edge* t = lcand->Onext();
-            kill(lcand);
+            subdivision->removeEdge(lcand);
             lcand = t;
         }
 
@@ -185,7 +152,7 @@ Edge* Delaunay::rCand(Edge* base)
 		while (inCircle(base->Dest(), base->Org(), rcand->Dest(), rcand->Oprev()->Dest()))
 		{
 			Edge* t = rcand->Oprev();
-			kill(rcand);
+			subdivision->removeEdge(rcand);
 			rcand = t;
 		}
 	}
@@ -213,14 +180,14 @@ void Delaunay::mergeLoop(Edge*& basel)
             && inCircle(lcand->Dest(), lcand->Org(), rcand->Org(), rcand->Dest())))
     {
 
-            basel = connect(rcand, basel->Sym());
+            basel = subdivision->connect(rcand, basel->Sym());
 
     }
     else
 
     {
 
-        basel = connect(basel->Sym(), lcand->Sym());
+        basel = subdivision->connect(basel->Sym(), lcand->Sym());
 
     }
 
@@ -271,14 +238,108 @@ EdgeHalves Delaunay::triangulate(const VertexList& points)
 
     mergeLoop(base);
 
+    gatherTriangles();
+
     return EdgeHalves({left_outer}, {right_outer});
 }
 
-QuadEdgeList Delaunay::getTriangulation()
+void Delaunay::getTriangulation()
 {
 
-    EdgeHalves edge_tuple = triangulate(vertices);
-    return edges;
+    EdgeHalves edge_tuple = triangulate(subdivision->getVertices());
+    getVoronoiDiagram();
+    gatherTriangles();
 
 }
+
+ std::string Delaunay::generateFileName()
+{
+    file_index++;
+    std::string filename_left = "./data/triangle_files/gathered_triangles_";
+    std::string filename_right = ".txt";
+    std::string file_number = std::to_string(file_index);
+    std::string filename = filename_left + file_number + filename_right;
+    return filename;
+}
+
+void Delaunay::gatherTriangles()
+{
+
+ std::ofstream triangle_file;
+ triangle_file.open(generateFileName());
+ std::unordered_set<Edge*> processedEdges;
+
+
+
+ for (uint32_t i = 0; i < subdivision->getMaxEdges()*2; i++)
+ {
+    Edge* edge = subdivision->getEdge(i/2);
+    if (!edge) continue;
+    if (i % 2) edge = edge->Sym();
+
+    Vertex* p1 = edge->Org();
+    Vertex* p2 = edge->Lnext()->Org();
+    Vertex* p3 = edge->Lprev()->Org();
+
+    if (!CCW(p1, p2, p3))
+        continue;
+    if (processedEdges.count(edge) != 0)
+        continue;
+
+    triangle_file << p1->getID() << ' ' << p2->getID() << ' ' << p3->getID() << std::endl;
+
+    processedEdges.insert(edge);
+    processedEdges.insert(edge->Lnext());
+    processedEdges.insert(edge->Lprev());
+ }
+ triangle_file.close();
+
+}
+
+void Delaunay::getVoronoiDiagram()
+{
+    for (uint32_t i = 0; i < subdivision->getNumEdges(); i++)
+    {
+        Edge* e = subdivision->getEdge(i);
+
+        if (CCW(e->Org(), e->Dest(), e->Onext()->Dest()) &&
+            CCW(e->Org(), e->Oprev()->Dest(), e->Dest()))
+        {
+           e->Rot()->setOrg(subdivision->circumCenter(e->Org(), e->Dest(), e->Onext()->Dest()));
+           e->InvRot()->setOrg(subdivision->circumCenter(e->Org(), e->Oprev()->Dest(), e->Dest()));
+        }
+
+    }
+
+    std::ofstream voronoi_file;
+    voronoi_file.open("./data/dual_vertices.txt");
+
+    const VertexList& voronoi_vertices = subdivision->getDualVertices();
+
+    std::unordered_set<Vertex*> processed_vertices;
+
+    for (Vertex* dual_vertex : voronoi_vertices)
+    {
+        if (processed_vertices.count(dual_vertex) != 0)
+        {
+            continue;
+        }
+        voronoi_file << dual_vertex->getX() << ' ' << dual_vertex->getY() << std::endl;
+        processed_vertices.insert(dual_vertex);
+    }
+
+    voronoi_file.close();
+}
+
+
+
+Delaunay::~Delaunay()
+{
+    delete subdivision;
+}
+
+
+
+
+
 
